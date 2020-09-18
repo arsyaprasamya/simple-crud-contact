90a96acf99b12760bab50bd87ac0b5f083872cba
Arsya <42167020+arsyaprasamya@users.noreply.github.com>
2020-09-18 13:55:30 +0700

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,8 @@
-export const ADD_USER = 'ADD_USER'
+export const ADD_USER = "ADD_USER";
 
 export function addUser(user) {
-  return
-    {
-      type: ADD_USER,
-      user;
-    }
+  return {
+    type: ADD_USER,
+    user,
+  };
 }
